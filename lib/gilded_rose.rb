6900323180e6b52c45c1cf68e33976bc7ca6a5c6
Alex Do <alexkdo03@gmail.com>
2024-02-27 20:44:58 -0600

    Created hash DEFAULT CLASS and SPECIALIZED CLASS and placed call in self.for method, re-added normal, brie, and backstage classes as well as initialize method that I removed on accident

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,25 +1,14 @@
 class GildedRose
 
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'normal'                                    =>  Normal,
+    'Aged Brie'                                 =>  Brie,
+    'Backstage passes to a TAFKAL80ETC concert' =>  Backstage }
+
   def self.for(name, quality, days_remaining)
-    @item = klass_for(name).new(quality, days_remaining)
-  end
-
-  def self.klass_for(name)
-    case name
-    when 'normal'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Item
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
-    end
-  end
-
-  class Item
-    def tick
-    end
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).
+      new(quality, days_remaining)
   end
 
 end
