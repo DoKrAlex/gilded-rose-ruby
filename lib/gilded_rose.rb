439f84a877492c1731b003bc09f612ea12f02db8
Alex Do <alexkdo03@gmail.com>
2024-02-27 20:35:20 -0600

    Removed inheritance hierachy and related classes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,54 +11,14 @@ class GildedRose
     when 'Aged Brie'
       Brie
     when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
+      Item
     when 'Backstage passes to a TAFKAL80ETC concert'
       Backstage
     end
   end
 
   class Item
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-  end
-
-  class Normal < Item
     def tick
-      @days_remaining -= 1
-      return if @quality == 0
-
-      @quality -= 1
-      @quality -= 1 if @days_remaining <= 0
-    end
-  end
-
-  class Brie < Item
-    def tick
-      @days_remaining -= 1
-      return if @quality >= 50
-
-      @quality += 1
-      @quality += 1 if @days_remaining <= 0
-    end
-  end
-
-  class Sulfuras < Item
-    def tick
-    end
-  end
-
-  class Backstage < Item
-    def tick
-      @days_remaining -= 1
-      return              if @quality >= 50
-      return @quality = 0 if @days_remaining < 0
-
-      @quality += 1
-      @quality += 1 if @days_remaining < 10
-      @quality += 1 if @days_remaining < 5
     end
   end
 
