01e8fa117ade7915a2c43f5154f03771b53eda3a
Alex Do <alexkdo03@gmail.com>
2024-02-27 20:10:39 -0600

    Added updated  tick methods for brie, sulfuras and backstage

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,11 +6,18 @@ class GildedRose
   end
 
   def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
+    @item = Brie.new(quality, days_remaining)
+    item.tick
+  end
 
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0
+  def sulfuras_tick
+    @item = Sulfuras.new(quality, days_remaining)
+    item.tick
+  end
+
+  def backstage_tick
+    @item = Backstage.new(quality, days_remaining)
+    item.tick
   end
 
   def quality
@@ -38,3 +45,50 @@ class GildedRose
       @quality -= 1 if @days_remaining <= 0
     end
   end
+
+  class Brie
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0
+    end
+  end
+
+  class Sulfuras
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+    end
+  end
+
+  class Backstage
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return              if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
+  end
+
+end
