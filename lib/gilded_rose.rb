5f1274133251644b0a82c9fbb737ba94785fa980
Alex Do <alexkdo03@gmail.com>
2024-02-27 19:57:45 -0600

    Created normal class with method tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,51 +1,22 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
-
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
-  end
-
-  def tick
-    case name
-    when 'normal'
-      return normal_tick
-    when 'Aged Brie'
-      return brie_tick
-    when 'Sulfuras, Hand of Ragnaros'
-      return sulfuras_tick
-    when 'Backstage passes to a TAFAL80ETC concert'
-      return backstage_tick
-    end
-  end
 
   def normal_tick
-    @day_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @day_remaining <= 0
+    @item = Normal.new(quality, days_remaining)
+    item.tick
   end
 
-  def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
+  class Normal
+    attr_reader :quality, :days_remaining
 
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
   end
-
-  def sulfuras_tick
-  end
-
-  def backstage_tick
-    @days_remaining -= 1
-    return              if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
-  end
-end
