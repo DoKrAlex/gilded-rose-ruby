665a57e002b45e2c78669d18eb56472c83f7c9f1
Alex Do <alexkdo03@gmail.com>
2024-02-27 20:00:20 -0600

    Added brie tick, quality, and days remaining methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,6 +5,24 @@ class GildedRose
     item.tick
   end
 
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
+
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
+  end
+
   class Normal
     attr_reader :quality, :days_remaining
 
